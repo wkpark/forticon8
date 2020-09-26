5e0763f93603c176eb4006938c254791e3fbe6dd
Won-Kyu Park <wkpark@gmail.com>
2020-09-26 19:37:12 +0900

    lovlap: cleanup using do/end do loop

--- lovlap.f
+++ lovlap.f
@@ -24,56 +24,71 @@ C
 C
 C    GENERATE FACTORIALS ONLY ONCE.
 C
-      IF(JGO) GO TO 10
-      JGO=.TRUE.
-      FACT(1)=1.D0
-      DO 5 I=2,25
- 5    FACT(I)=FACT(I-1)*DFLOAT(I-1)
- 10   CONTINUE
+      IF(.NOT.JGO) Then
+        JGO=.TRUE.
+        FACT(1)=1.D0
+        DO I=2,25
+          FACT(I)=FACT(I-1)*DFLOAT(I-1)
+        End Do
+      End If
+C
       M2=M1
       STRAD=0.D0
       RHOA=R*SK1
       RHOB=R*SK2
-      TERMA=0.5D0**(L1+L2+1) * DSQRT(DFLOAT((L1+L1+1)*(L2+L2+1))*
-     1 FACT(L1-M1+1)*FACT(L2-M1+1)/(FACT(N1+N1+1)*FACT(N2+N2+1)*
-     2 FACT(L1+M1+1)*FACT(L2+M1+1))*RHOA**(N1+N1+1)*RHOB**(N2+N2+1))
+      TERMA=0.5D0**(L1+L2+1) * DSQRT(
+     & DFLOAT((L1+L1+1)*(L2+L2+1))
+     & *FACT(L1-M1+1)*FACT(L2-M1+1)/
+     & (FACT(N1+N1+1)*FACT(N2+N2+1)*FACT(L1+M1+1)*FACT(L2+M1+1))
+     & *RHOA**(N1+N1+1)*RHOB**(N2+N2+1))
+
       JEND=1+((L1-M1)/2)
       KEND=1+((L2-M2)/2)
       IEB=M1+1
-      DO 50 J=1,JEND
-      JU=J-1
-      IAB=N1-L1+JU+JU+1
-      ICB=L1-M1-JU-JU+1
-      CON1=FACT(L1+L1-JU-JU+1)/(FACT(L1-M1-JU-JU+1)*FACT(JU+1)*
-     1 FACT(L1-JU+1))
-      DO 50 K=1,KEND
-      KU=K-1
-      CON12=CON1*FACT(L2+L2-KU-KU+1)/(FACT(L2-M2-KU-KU+1)*FACT(KU+1)*
-     1 FACT(L2-KU+1))
-      IEV=JU+KU+L2
-      IF(2*(IEV/2).NE.IEV) CON12=-CON12
-      IBB=N2-L2+KU+KU+1
-      IDB=L2-M2-KU-KU+1
-      VALUE=0.D0
-      DO 90 I6=1,IEB
-      DO 90 I5=1,IEB
-      VALUE1=BINCOE(IEB,I6)*BINCOE(IEB,I5)
-      IEV=I5+I6
-      IF(2*(IEV/2).NE.IEV) VALUE1=-VALUE1
-      DO 90 I4=1,IDB
-      VALUE1=-VALUE1
-      VALUE2=BINCOE(IDB,I4)*VALUE1
-      DO 90 I3=1,ICB
-      VALUE3=BINCOE(ICB,I3)*VALUE2
-      DO 90 I2=1,IBB
-      VALUE3=-VALUE3
-      VALUE4=BINCOE(IBB,I2)*VALUE3
-      DO 90 I1=1,IAB
-      TERM=VALUE4*BINCOE(IAB,I1)
-      IR=I1+I2+IEB+IEB-I6-I6-I3+IDB-I4+ICB-1
-      IP=IAB-I1+IBB-I2+IEB+IEB-I5-I5+ICB-I3+IDB-I4+1
- 90   VALUE=VALUE+A(IP)*B(IR)*TERM
- 50   STRAD=STRAD+VALUE*CON12
+
+      DO J=1,JEND
+        JU=J-1
+        IAB=N1-L1+JU+JU+1
+        ICB=L1-M1-JU-JU+1
+        CON1=FACT(L1+L1-JU-JU+1)/(FACT(L1-M1-JU-JU+1)
+     &   *FACT(JU+1)*FACT(L1-JU+1))
+        DO K=1,KEND
+          KU=K-1
+          CON12=CON1*FACT(L2+L2-KU-KU+1)/(FACT(L2-M2-KU-KU+1)
+     &     *FACT(KU+1)*FACT(L2-KU+1))
+          IEV=JU+KU+L2
+          IF(2*(IEV/2).NE.IEV) CON12=-CON12
+          IBB=N2-L2+KU+KU+1
+          IDB=L2-M2-KU-KU+1
+          VALUE=0.D0
+          DO I6=1,IEB
+            DO I5=1,IEB
+              VALUE1=BINCOE(IEB,I6)*BINCOE(IEB,I5)
+              IEV=I5+I6
+              IF(2*(IEV/2).NE.IEV) VALUE1=-VALUE1
+              DO I4=1,IDB
+                VALUE1=-VALUE1
+                VALUE2=BINCOE(IDB,I4)*VALUE1
+                DO I3=1,ICB
+                  VALUE3=BINCOE(ICB,I3)*VALUE2
+                  DO I2=1,IBB
+                    VALUE3=-VALUE3
+                    VALUE4=BINCOE(IBB,I2)*VALUE3
+                    DO I1=1,IAB
+                      TERM=VALUE4*BINCOE(IAB,I1)
+                      IR=I1+I2+IEB+IEB-I6-I6-I3+IDB-I4+ICB-1
+                      IP=IAB-I1+IBB-I2+IEB+IEB-I5-I5+ICB-I3+IDB-I4+1
+                      VALUE=VALUE+A(IP)*B(IR)*TERM
+                    End Do
+                  End Do
+                End Do
+              End Do
+            End Do
+          End Do
+          STRAD=STRAD+VALUE*CON12
+        End Do
+      End Do
+
       STRAD=STRAD*TERMA
       RETURN
       END
