98eb2e16b11797c17d3d6d8203abfae5a4001077
Won-Kyu Park <wkpark@gmail.com>
2020-09-27 18:17:34 +0900

    abfns: use do/end do loop with cleanup

--- abfns.f
+++ abfns.f
@@ -1,8 +1,8 @@
       SUBROUTINE ABFNS(A,B)
       IMPLICIT REAL*8(A-H,O-Z)
       PARAMETER (MXUSER=230)
-      PARAMETER (MXUSR2=231)
-      DIMENSION A(MXUSER),B(MXUSER)
+      Parameter (SMALL=1.0D-7)
+      DIMENSION A(*),B(*)
       COMMON/LOCLAP/SK1,SK2,RR,L1,L2,M,N1,N2,MAXCAL
 C
 C    SUBROUTINE FOR CALCULATING A AND B FUNCTIONS FOR USE IN LOVLAP.
@@ -10,17 +10,33 @@ C
       J=MAXCAL+1
       RHO1=0.5D0*(SK1+SK2)*RR
       RHO2=0.5D0*(SK1-SK2)*RR
-      IF(DABS(RHO1).GT.165.D0) GO TO 100
-      IF(DABS(RHO2).GT.165.D0) GO TO 100
+      IF((DABS(RHO1).GT.165.D0).OR.(DABS(RHO2).GT.165.D0)) Then
+C
+C    IF THE ARGUMENT IS ZERO A SEPARATE FORMULA MUST BE USED.
+C
+        DO I=1,MXUSER
+          A(I)=0.D0
+          B(I)=0.D0
+        End Do
+        Return
+      End If
       C=DEXP(-RHO1)
       A(1)=C/RHO1
-      DO 15 I=2,J
- 15   A(I)=(DFLOAT(I-1)*A(I-1)+C)/RHO1
+      DO I=2,J
+        A(I)=(DFLOAT(I-1)*A(I-1)+C)/RHO1
+      End Do
       IX=J
-      IR=DABS(2.*RHO2)
+      IR=DABS(2.D0*RHO2)
       IS=MIN0(IR+1,19)
-      IF(RHO2) 25,35,25
- 25   D=DEXP(RHO2)
+      IF(RHO2.EQ.0) Then
+        DO I=1,IX,2
+          B(I)=2.D0/DFLOAT(I)
+          B(I+1)=0.D0
+        End Do
+        Return
+      End If
+
+      D=DEXP(RHO2)
       H=1.D0/D
 C
 C    USE THE DSINH ROUTINE INSTEAD OF SUMMING THE INFINITE SERIES.
@@ -31,51 +47,47 @@ C    AS MANY SUCCESSIVE B-FUNCTIONS ARE GENERATED FROM B-0 BY THE
 C    RECURRENCE FORMULAE AS ACCURACY WILL PERMIT.
 C
       B(1)=R/RHO2
-      DO 51 I=2,IX,IS
-      IF(IR.EQ.0) GO TO 40
-      IL=IS-1
+      DO I=2,IX,IS
+        IF(IR.NE.0) Then
 C
 C    MODIFICATION TO AVOID EXCEEDING STORAGE LIMITS.
 C    D. WALLACE 04/14/71
 C
-      DO 31 K=I,IX
-      IF((-1)**K) 29,29,30
- 29   B(K)=(R+DFLOAT(K-1)*B(K-1))/RHO2
-      GO TO 31
- 30   B(K)=-(D+H-DFLOAT(K-1)*B(K-1))/RHO2
- 31   CONTINUE
- 40   IN=I+IS-1
+          DO K=I,IX
+            !IF(((-1)**K).LT.0) Then
+            IF(MOD(K,2).NE.0) Then
+              B(K)=( R  +DFLOAT(K-1)*B(K-1))/RHO2
+            Else
+              B(K)=(-D-H+DFLOAT(K-1)*B(K-1))/RHO2
+            End If
+          End Do
+        End If
+        IN=I+IS-1
 C
 C    AFTER THE RECURRENCE FORMULAE HAVE BEEN APPLIED AN APPROPRIATE
 C    NUMBER OF TIMES THE NEXT B-FUNCTION IS OBTAINED BY SUMMATION
 C    OF THE INFINITE SERIES.
 C
-      IF(IN-IX) 39,39,38
- 39   IF((-1)**IN) 44,44,42
- 42   TR=RHO2
- 105  B(IN)=-2.*TR/DFLOAT(IN+1)
-      DO 43 J=1,500
-      TR=TR*RHO2**2/DFLOAT((2*J)*(2*J+1))
-      IF(DABS(TR/B(IN))-1.0D-7 ) 51,51,43
- 43   B(IN)=B(IN)-2.*TR/DFLOAT(IN+1+2*J)
-      GO TO 51
- 44   TR=1.
- 107  B(IN)=2.*TR/DFLOAT(IN)
-      DO 46 J=1,500
-      TR=TR*RHO2**2/DFLOAT((2*J)*(2*J-1))
-      IF(DABS(TR/B(IN))-1.0D-7 ) 51,51,46
- 46   B(IN)=B(IN)+2.*TR/DFLOAT(IN+2*J)
- 51   CONTINUE
-C
-C    IF THE ARGUMENT IS ZERO A SEPARATE FORMULA MUST BE USED.
-C
-      GO TO 38
- 35   DO 36 I=1,IX,2
-      B(I)=2.D0/DFLOAT(I)
- 36   B(I+1)=0.D0
- 38   RETURN
- 100  DO 101 I=1,MXUSER
-      A(I)=0.D0
- 101  B(I)=0.D0
-      GO TO 38
+        IF(IN.GT.IX) Return
+        !IF((-1)**IN.GT.0) Then
+        IF(MOD(IN,2).EQ.0) Then
+          TR=RHO2
+          B(IN)=-2.D0*TR/DFLOAT(IN+1)
+          DO J=1,100
+            TR=TR*RHO2**2/DFLOAT((2*J)*(2*J+1))
+            IF(DABS(TR/B(IN)).LE.SMALL) GoTo 51
+            B(IN)=B(IN)-2.D0*TR/DFLOAT(IN+1+2*J)
+          End Do
+        Else
+          TR=1.D0
+          B(IN)=2.D0*TR/DFLOAT(IN)
+          DO J=1,100
+            TR=TR*RHO2**2/DFLOAT((2*J)*(2*J-1))
+            IF(DABS(TR/B(IN)).LE.SMALL) GoTo 51
+            B(IN)=B(IN)+2.D0*TR/DFLOAT(IN+2*J)
+          End Do
+        End If
+ 51     CONTINUE
+      End Do
+      RETURN
       END
